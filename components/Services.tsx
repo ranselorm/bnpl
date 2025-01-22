d60f202fb4c9a6b50e652c28c93bfea789cae828
ranselorm <ranselorm@outlook.com>
2025-01-22 18:44:49 +0000

    Modify services

--- components/Services.tsx
+++ components/Services.tsx
@@ -15,7 +15,7 @@ const ServiceCard = ({
   description,
   buttonLabel,
 }: ServiceCardProps) => (
-  <div className="flex flex-col items-center text-center">
+  <div className="flex flex-col items-center text-center w-[350px] h-full justify-between">
     <div className="w-40 h-40 mb-4">
       <Image
         src={imageSrc}
@@ -25,9 +25,9 @@ const ServiceCard = ({
         className="object-contain"
       />
     </div>
-    <h3 className="text-xl font-semibold mb-2">{title}</h3>
-    <p className="text-gray-600 mb-4">{description}</p>
-    <button className="px-6 py-2 border-2 border-black rounded hover:bg-gray-200 transition">
+    <h3 className="text-xl font-bold mb-2">{title}</h3>
+    <p className="text-gray-600 mb-4 flex-1 my-4">{description}</p>
+    <button className="px-6 py-2 border border-primary rounded-full hover:bg-gray-200 transition">
       {buttonLabel}
     </button>
   </div>
@@ -37,7 +37,7 @@ const ServicesSection = () => {
   const services = [
     {
       imageSrc: "/icons/icon1.png",
-      title: "Buy Now, Pay Later: Credit",
+      title: "CCF Buy Now, Pay later: Credit",
       description:
         "Select an item, make a down payment, get your item and enjoy the flexibility of paying over time.",
       buttonLabel: "Learn more",
@@ -46,32 +46,35 @@ const ServicesSection = () => {
       imageSrc: "/icons/icon2.png",
       title: "Pay4Me: Peer-to-peer financing",
       description:
-        "Get the support of a caring community with Pay4Me. Let your family and friends help you pay for the things you need.",
+        "Let your family and friends support you with Pay4Me. Join a caring community to help you achieve your goals with ease and convenience.",
       buttonLabel: "Learn more",
     },
     {
       imageSrc: "/icons/icon3.png",
       title: "KeepMyPrice: 30-days layaway",
       description:
-        "Protect yourself from inflation. Lock in the price of an item with a deposit for 30 days and self-finance your purchase.",
+        "Secure your item price with a deposit for 30 days. Protect yourself from inflation while managing your finances effectively.",
       buttonLabel: "Learn more",
     },
   ];
 
   return (
     <section className="bg-white py-16 text-center">
-      <div className="container mx-auto">
+      <div className="container mx-auto px-4">
         {/* Heading */}
-        <h2 className="text-3xl md:text-4xl font-bold mb-4">
-          Level up your buying power and unlock your dream
-        </h2>
-        <p className="text-gray-600 mb-12">
-          Ready to unlock your dreams? Motito has your back! We're here to show
-          you fresh ways to get what you want, exactly when you want it.
-        </p>
+        <div className="mx-auto md:max-w-3xl w-full text-center">
+          <h2 className="text-3xl md:text-5xl font-bold mb-4 w-full">
+            Discover What We <span className="text-primary">Offer</span>
+          </h2>
+          <p className="text-gray-600 mb-12 text-lg">
+            Elevate your e-commerce experience with our powerful feature set
+            designed for unparalleled excellence. Show you fresh ways to get
+            what you want, exactly when you want it.
+          </p>
+        </div>
 
         {/* Services */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 place-items-center">
           {services.map((service, index) => (
             <ServiceCard
               key={index}
